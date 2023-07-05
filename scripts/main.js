028acbaf5c6e6dc1b3c4534e1ea71cd877000472
sticky-cheng <102734320+sticky-cheng@users.noreply.github.com>
2023-07-05 09:03:00 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
+let count = 0;
+
 function changeImage(src) {
     document.querySelector("img").src = "../images/" + src + ".png";
 }
 
 document.body.onmousedown = function () {
+    count += 1;
+    document.querySelector("#pop").textContent = count;
     changeImage("unko_gorilla");
 }
 document.body.onmouseup = function () {
